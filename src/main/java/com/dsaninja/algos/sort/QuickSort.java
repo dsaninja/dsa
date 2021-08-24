11227d78207302b1db1367e1f8e7676129e2b49c
dsaninja <dsaninjablog@gmail.com>
2021-08-24 19:14:50 +0530

    updated for quick sort

---
+++ src/main/java/com/dsaninja/algos/sort/QuickSort.java
@@ -0,0 +1,68 @@
+package com.dsaninja.algos.sort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The quick sort algorithm implemented in java. The idea is based on the fact that
+ * an element is said to be in sorted position if all the elements to its left are
+ * smaller than it and all the elements to its right are more than it.
+ *
+ * @author gaurs
+ */
+public class QuickSort{
+
+    public void sort(int[] input){
+        quickSort(input, 0, input.length - 1);
+    }
+
+    private void quickSort(int[] input, int start, int end){
+        if(start < end){
+            // find partition location
+            int partitionIndex = partition(input, start, end);
+            // sort first half
+            quickSort(input, start, partitionIndex);
+            // sort second half
+            quickSort(input, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(int[] input, int start, int end){
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while(i < j){
+            // move toward right until you find an element more than pivot
+            // stop there as that element needs to be moved to the right of pivot
+            while(input[i] <= pivot){
+                i++;
+            }
+
+            // move toward left until you find an element smaller than pivot
+            // stop there as that element needs to be moved to the left of pivot
+            while(input[j] > pivot){
+                j--;
+            }
+
+            // if i < j; swap these
+            if(i < j){
+                int temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+            }
+        }
+
+        // the j will be pointing to the location meant for pivot
+        // as all the elements left to j are smaller than pivot
+        // and all the elements to the right of j are more than pivot
+        int temp = input[j];
+        input[j] = input[start];
+        input[start] = temp;
+
+        return j;
+    }
+}
---
+++ src/test/groovy/com/dsaninja/algos/sort/QuickSortSpec.groovy
@@ -0,0 +1,41 @@
+package com.dsaninja.algos.sort
+
+import spock.lang.Specification
+
+class QuickSortSpec extends Specification {
+
+    def quickSort = new QuickSort()
+
+    def "test sorting an un-sorted input array"() {
+        given: "an un-sorted input array"
+        def input = new int[]{1,5,3,9,2,4}
+
+        when: "selection sort is triggered"
+        quickSort.sort(input)
+
+        then: "array should be sorted in ascending order"
+        input == new int[]{1,2,3,4,5,9}
+    }
+
+    def "test sorting an sorted input array"() {
+        given: "an un-sorted input array"
+        def input = new int[]{1,2,3,4,5,9}
+
+        when: "selection sort is triggered"
+        quickSort.sort(input)
+
+        then: "array should be sorted in ascending order"
+        input == new int[]{1,2,3,4,5,9}
+    }
+
+    def "test sorting an empty input array"() {
+        given: "an un-sorted input array"
+        def input = new int[]{}
+
+        when: "selection sort is triggered"
+        quickSort.sort(input)
+
+        then: "array should remain unchanged"
+        input == new int[]{}
+    }
+}
